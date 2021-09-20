683aa64ea23589de5e7b694aeea4f952c8d1ca48
AAKASH PATEL <aakashpate6250@gmail.com>
2021-09-20 12:36:04 +0100

    First commit

---
+++ 3.Character Array/10_strncpy.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+
+    char str1[100];
+    char str2[100];
+    int n;
+
+    cout<<"Enter the first string : ";
+    cin>>str1;
+    cout<<endl;
+    cout<<"Enter the second string : ";
+    cin>>str2;
+    cout<<endl;
+    cout<<"Enter the number of character you want to copy : ";
+    cin>>n;
+    cout<<endl;
+
+    cout<<"Before copying the nth value : "<<endl;
+    cout<<"value of str1 "<<str1<<endl;
+    cout<<"value of str2 "<<str2<<endl;
+
+    strncpy(str1,str2,n);
+    
+    cout<<"After copying the nth value : "<<endl;
+    cout<<"value of str1 "<<str1<<endl;
+    cout<<"value of str2 "<<str2<<endl;
+
+    return 0;
+    
+}
